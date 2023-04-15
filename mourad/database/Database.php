4b6409786a33c886e17826ecf9c0b7c17393f466
mourad <101376159+mourad-edine@users.noreply.github.com>
2023-04-15 19:10:55 +0300

    wesh fa tsy mandeha Model Controller

---
+++ mourad/database/Database.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Data;
+
+use Exception;
+
+class Database{
+    private $usernama;
+    private $mdp;
+    private $server;
+    private $dname;
+
+
+    public function __construct()
+    {
+        
+    }
+
+    public  function connect(){
+        try{
+
+        }
+        catch(Exception $e){
+
+        }
+    }
+}
